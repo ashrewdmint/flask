757a2f088fc0778579b81743257eafb88e761969
Andrew Smith <andrew.caleb.smith@gmail.com>
2009-12-31 13:06:06 -0700

    Added Item, Inventory and Player

--- adventure.rb
+++ adventure.rb
@@ -274,3 +274,77 @@ class Adventure < Hallway
     end
   end
 end
+
+class Item
+  attr_reader :name, :description
+  include Nameable
+  
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+end
+
+class Inventory
+  attr_reader :items
+  
+  def initialize
+    @items = []
+  end
+  
+  def <<(item)
+    if item.is_a?(Item)
+      @items << item
+    else
+      false
+    end
+  end
+  
+  def push(item)
+    self << item
+  end
+  
+  def [](name)
+    @items.each do |i|
+      return i if i.name_matches? name
+    end
+    nil
+  end
+  
+  def find(name)
+    self[name]
+  end
+  
+  def delete_at(*names)
+    names.each do |name|
+      @items.delete_if do |i|
+        i.name_matches? name
+      end
+    end
+  end
+  
+  def give(name, inventory)
+    if item = self[name] and inventory.is_a?(Inventory)
+      delete_at(name)
+      inventory << item
+    end
+  end
+  
+  def show
+    return false if @items.length == 0
+    @items.each do |item|
+      puts
+      puts item.name
+      puts item.description
+    end
+  end
+end
+
+class Player
+  attr_reader :inventory
+  attr_accessor :name
+  
+  def initialize
+    @inventory = Inventory.new
+  end
+end
