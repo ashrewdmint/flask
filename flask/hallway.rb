e7eddab2820901e166e6e049ce777c9d89e2b077
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-01-02 12:22:32 -0700

    Put Hallway in its own file

---
+++ flask/hallway.rb
@@ -0,0 +1,49 @@
+module Flask
+
+  # A Hallway is a group of rooms
+  
+  class Hallway
+    attr_reader :rooms, :current_room
+
+    def initialize()
+      @rooms = []
+    end
+
+    def <<(sym)
+      room = Hallway.sym_to_class(sym).new(self)
+      @rooms << room if room.is_a?(Room)
+    end
+
+    def push(room)
+      self << room
+    end
+
+    def [](name)
+      @rooms.each do |r|
+        return r if r.name_matches? name
+      end
+      nil
+    end
+
+    def find(name)
+      self[name]
+    end
+
+    def enter(name)
+      @current_room.leave if @current_room
+  
+      unless @current_room = self[name]
+        self << name
+        @current_room = @rooms.last
+      end
+  
+      @current_room.enter
+    end
+
+    def self.sym_to_class(sym)
+      constant = sym.to_s.split('_').collect { |word| word.capitalize }.join
+      Object.const_get(constant)
+    end
+  end
+  
+end
--- flask/room.rb
+++ flask/room.rb
@@ -91,49 +91,5 @@ module Flask
       end
     end
   end
-
-  class Hallway
-    attr_reader :rooms, :current_room
-  
-    def initialize()
-      @rooms = []
-    end
-  
-    def <<(sym)
-      room = Hallway.sym_to_class(sym).new(self)
-      @rooms << room if room.is_a?(Room)
-    end
-  
-    def push(room)
-      self << room
-    end
-  
-    def [](name)
-      @rooms.each do |r|
-        return r if r.name_matches? name
-      end
-      nil
-    end
-  
-    def find(name)
-      self[name]
-    end
-  
-    def enter(name)
-      @current_room.leave if @current_room
-    
-      unless @current_room = self[name]
-        self << name
-        @current_room = @rooms.last
-      end
-    
-      @current_room.enter
-    end
-  
-    def self.sym_to_class(sym)
-      constant = sym.to_s.split('_').collect { |word| word.capitalize }.join
-      Object.const_get(constant)
-    end
-  end
   
 end
