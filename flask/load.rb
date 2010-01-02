9b228702b825ea5aef1ee94108d969b58b3fe15b
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-01-01 21:36:57 -0700

    Load class loads room data and instantiates new room classes

--- flask/load.rb
+++ flask/load.rb
@@ -9,6 +9,9 @@ module Flask
   end
   
   class Load
+    ROOM_RESERVED_KEYS = %w(exits items)
+    ROOM_DIRECTIONS = %w(north east south west)
+    
     def self.items(path)
       items = YAML.load_file(path)
       items.each_pair do |name, hash|
@@ -19,7 +22,38 @@ module Flask
     def self.rooms(path)
       rooms = YAML.load_file(path)
       rooms.each_pair do |name, hash|
-        #puts name
+        data = {}
+        reserved = {}
+
+        # Collect data
+        hash.each_pair do |key, value|
+          if ROOM_RESERVED_KEYS.include? key
+            reserved[key] = value
+          else
+            data[key.downcase.to_sym] = value.chomp
+          end
+        end
+
+        # Add items to room
+        if items = reserved['items']
+          items.each do |item|
+            # Do something with the item
+          end
+        end
+
+        # Add exits
+        if exits = reserved['exits']
+          exits.each_pair do |direction, info|
+            # Do something with the exit
+          end
+        end
+
+        # Create class
+        room_class = Class.new(Room) do
+          @data = data
+        end
+
+        Object.const_set(name, room_class)
       end
     end
   end
