10a8908adcf3032bddbae19c2d9eb3f6073305e7
Andrew Smith <andrew.caleb.smith@gmail.com>
2010-01-02 22:13:06 -0700

    Fixed flask.rb to require the "load" file.

--- flask.rb
+++ flask.rb
@@ -1,6 +1,6 @@
 # Load all flask files, in the correct order
 
-files = %w(modules modules responder inventory room hallway player adventure)
+files = %w(load modules responder inventory room hallway player adventure)
 
 files.each do |file|
   require File.dirname(__FILE__) + '/flask/' + file
